e727683667934397c019ddedc9d1d28217e2b0a9
Cosmo <cosmosnoob001@gmail.com>
2024-06-11 11:32:18 +0200

    Readme & FPS limiter weggehaald

--- src/js/game.js
+++ src/js/game.js
@@ -5,12 +5,11 @@ import { Resources, ResourceLoader } from './resources.js'
 export class Game extends Engine {
 
     constructor() {
-        super({ 
+        super({
             width: 1280,
             height: 720,
-            maxFps: 60,
             displayMode: DisplayMode.FitScreen
-         })
+        })
         this.start(ResourceLoader).then(() => this.startGame())
     }
 
@@ -19,7 +18,7 @@ export class Game extends Engine {
         const fish = new Actor()
         fish.graphics.use(Resources.Fish.toSprite())
         fish.pos = new Vector(400, 300)
-        fish.vel = new Vector(-10,0)
+        fish.vel = new Vector(-10, 0)
         this.add(fish)
     }
 }
