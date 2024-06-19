ac037e6bf2168b296c02823f27b47afe298f8867
roshanmomand <1028086@hr.nl>
2024-06-19 14:02:38 +0200

    LEADER BOARD EINDELIJK DONE

--- src/js/highscoredisplay.js
+++ src/js/highscoredisplay.js
@@ -1,4 +1,4 @@
-import { ScreenElement, GraphicsGroup } from "excalibur";
+import { ScreenElement, GraphicsGroup, Vector, Label, Font, Color } from "excalibur";
 import { gameTimer } from "./gameTimer"
 import { StartScreen } from "./startScreen";
 
@@ -8,7 +8,8 @@ export class Highscore extends ScreenElement {
     }
     pName
     pTime
-    onInitialize() {
+    static playerScores = [];
+    onInitialize(engine) {
         console.log('HighScore Class is loaded');
 
         // haal de highscores op uit de localstorage
@@ -33,21 +34,36 @@ export class Highscore extends ScreenElement {
             for (let i = 1; i < highScoreList.length; i++) {
                 let cur = highScoreList[i]
                 let prev = highScoreList[i - 1]
-                console.log(cur,prev);
+                console.log(cur, prev);
                 if (cur.TimeNumber < prev.TimeNumber) {
                     console.log("swap");
                     highScoreList[i] = prev
-                    highScoreList[i-1] = cur;
+                    highScoreList[i - 1] = cur;
                     isSorted = false
                 }
             }
-        
+
         }
-        highScoreList = highScoreList.slice(0,5)
+        highScoreList = highScoreList.slice(0, 5)
         console.log("eee")
         localStorage.setItem("highscore", JSON.stringify(highScoreList))
+        Highscore.playerScores = highScoreList
 
+        for (let i = 0; i < highScoreList.length; i++) {
 
+            let playerInfo = new Label({
+                text: highScoreList[i].PlayerName+ "  " + highScoreList[i].PlayerTime,
+                pos: new Vector(engine.drawWidth / 2, engine.drawHeight  / 2 - 100 + (i*75)),
+                font: new Font({
+                    size: 50,
+                    family: 'impact'
+                }),
+                color: Color.White
+            })
+            playerInfo.anchor = new Vector(0.5, 0.5)
+            this.scene?.add(playerInfo)
+            
+        }
 
 
         let hDisplayGraphics = new GraphicsGroup({
