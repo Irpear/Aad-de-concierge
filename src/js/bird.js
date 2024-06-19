c8e21967c970b3219a87190abb248978db60fdc1
Irpear <145675108+Irpear@users.noreply.github.com>
2024-06-19 12:22:53 +0200

    vogelanimatiehopelijk

--- src/js/bird.js
+++ src/js/bird.js
@@ -6,24 +6,34 @@ import { mathFunction } from "./mathFunctions";
 export class Bird extends Actor {
     canDespawn = false;
     dir = 1;
-    constructor(position,dir) {
+    constructor(position, dir) {
         super()
         this.dir = dir;
         this.pos = position;
         this.pos.x *= dir;
+
+        const runSheet = SpriteSheet.fromImageSource({
+            image: Resources.Duck,
+            grid: { rows: 2, columns: 5, spriteWidth: 120, spriteHeight: 117 }
+        })
+
+        const fly = Animation.fromSpriteSheet(runSheet, range(0, 8), 100)
+
+        this.graphics.add("fly", fly)
+        this.graphics.use('fly')
     }
     onInitialize(engine) {
 
         console.log("Bird is created")
 
         let spr = Resources.Bird.toSprite();
-        this.scale = new Vector(this.dir*0.33, 0.33);
+        this.scale = new Vector(this.dir * 0.33, 0.33);
 
         this.graphics.use(spr);
 
         // this.pos = new Vector(500, 300)
 
-        this.vel = new Vector(this.dir*-mathFunction.Lerp(600,1000,Math.random()), 0);
+        this.vel = new Vector(this.dir * -mathFunction.Lerp(600, 1000, Math.random()), 0);
         let col = Shape.Circle(128)
         this.body.collisionType = CollisionType.Passive;
         this.collider.set(col);
