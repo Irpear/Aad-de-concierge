19ee317e0b6e9acf22b6482ea00868572778ce29
Cosmo <cosmosnoob001@gmail.com>
2024-06-24 13:31:08 +0200

    Gamepad controls startscreen

--- src/js/startScreen.js
+++ src/js/startScreen.js
@@ -1,13 +1,13 @@
-// @ts-nocheck
-
-import { Actor, Color, Font, Keys, Label, Scene, Timer, Vector } from "excalibur";
+import { Actor, Axes, Buttons, Color, Font, Keys, Label, Scene, Timer, Vector } from "excalibur";
 import { NameInput } from "./nameInput";
 import { projectileSpawner } from "./projectileSpawner";
 import { birdSpawner } from "./birdSpawner";
 import { Player } from "./player";
 import { Endscene } from "./endScene";
+import { Game } from "./game";
 
 export class StartScreen extends Scene {
+    game;
     inputs = [];
     static selectedDifficulty = -1;
     static playerName;
@@ -15,8 +15,14 @@ export class StartScreen extends Scene {
     difficultyActors = [];
     difficultyConfirmed = false;
     blinkTimer;
+    gpadX = 0
+    gpadY = 0
+    bpress = false
+    stickMovedX = false
+    stickMovedY = false
 
     onInitialize() {
+
         this.blinkTimer = new Timer({
             fcn: () => this.blinkingHandler(),
             repeats: true,
@@ -129,10 +135,6 @@ export class StartScreen extends Scene {
         startButtonText.anchor = new Vector(0.5, 0.5);
         startButton.addChild(startButtonText);
 
-        // Add an event listener for the start button
-        startButton.on('pointerup', () => this.startGame()
-        );
-
         // Add actors
         this.add(title);
         this.add(subtitle);
@@ -142,7 +144,12 @@ export class StartScreen extends Scene {
     }
 
     onPreUpdate(engine) {
-        if (engine.input.keyboard.wasPressed(Keys.Right)) {
+        if (Game.gpad != null) {
+            this.gpadX = Game.gpad.getAxes(Axes.LeftStickX);
+            this.gpadY = Game.gpad.getAxes(Axes.LeftStickY);
+            this.bpress = Game.gpad.isButtonPressed(Buttons.Face1);
+        }
+        if (engine.input.keyboard.wasPressed(Keys.Right) || this.gpadX > 0 && this.stickMovedX === false) {
             if (this.selectedLetterSlot < 6 && !this.difficultyConfirmed) {
                 this.selectedLetterSlot++;
             }
@@ -150,7 +157,7 @@ export class StartScreen extends Scene {
                 StartScreen.selectedDifficulty++;
             }
         }
-        if (engine.input.keyboard.wasPressed(Keys.Left)) {
+        if (engine.input.keyboard.wasPressed(Keys.Left) || this.gpadX < 0 && this.stickMovedX === false) {
             if (this.selectedLetterSlot > 0 && !this.difficultyConfirmed) {
                 this.selectedLetterSlot--;
             }
@@ -178,7 +185,7 @@ export class StartScreen extends Scene {
             StartScreen.selectedDifficulty = -1
         }
 
-        if (engine.input.keyboard.wasPressed(Keys.Down)) {
+        if (engine.input.keyboard.wasPressed(Keys.Down) || this.gpadY > 0 && this.stickMovedY === false) {
             if (StartScreen.selectedDifficulty >= 0) {
                 const startButton = this.actors.find(actor => actor.hasTag('startButton'));
                 startButton.color = Color.Green
@@ -207,16 +214,29 @@ export class StartScreen extends Scene {
 
             }
         }
-        if (engine.input.keyboard.wasPressed(Keys.Up) && this.difficultyConfirmed === true) {
+        if (engine.input.keyboard.wasPressed(Keys.Up) || this.gpadY < 0 && this.stickMovedY === false && this.difficultyConfirmed === true) {
             const startHelpText = this.actors.find(actor => actor.hasTag('startHelpText'));
-            this.difficultyConfirmed = false
-            this.blinkTimer.stop()
-            startHelpText.text = ""
-            const startButton = this.actors.find(actor => actor.hasTag('startButton'));
-            startButton.color = Color.Gray
+            if (startHelpText) {
+                this.difficultyConfirmed = false
+                this.blinkTimer.stop()
+                startHelpText.text = ""
+                const startButton = this.actors.find(actor => actor.hasTag('startButton'));
+                startButton.color = Color.Gray
+            }
         }
-        if (engine.input.keyboard.wasPressed(Keys.Enter) && this.difficultyConfirmed === true) {
+        if (engine.input.keyboard.wasPressed(Keys.Enter) || this.bpress && this.difficultyConfirmed === true) {
             this.startGame();
+        }
+
+        if (this.gpadX != 0) {
+            this.stickMovedX = true;
+        } else {
+            this.stickMovedX = false;
+        }
+        if (this.gpadY != 0) {
+            this.stickMovedY = true;
+        } else {
+            this.stickMovedY = false;
         }
     }
     blinkingHandler() {
