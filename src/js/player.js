7228c6f69b55c7798c164ca355ccffb33237874e
Cosmo <cosmosnoob001@gmail.com>
2024-06-25 13:02:10 +0200

    Fix inverted controls goku mode GPAD

--- src/js/player.js
+++ src/js/player.js
@@ -1,4 +1,4 @@
-import { Actor, CollisionType, Keys, Vector, Shape, Debug, Physics, Ray, Animation, range, SpriteSheet, Graphic, ConsoleAppender, clamp,Axes,Buttons } from "excalibur"
+import { Actor, CollisionType, Keys, Vector, Shape, Debug, Physics, Ray, Animation, range, SpriteSheet, Graphic, ConsoleAppender, clamp, Axes, Buttons } from "excalibur"
 import { Resources } from "./resources"
 import { mathFunction } from "./mathFunctions"
 import { vector } from "excalibur/build/dist/Util/DrawUtil"
@@ -18,9 +18,9 @@ export class Player extends Actor {
     kbVel;
     static playerPos;
     static playerVel;
-    static isGoku=false;
+    static isGoku = false;
 
-    landed=true;
+    landed = true;
     constructor() {
         super({
             width: 100,
@@ -47,16 +47,14 @@ export class Player extends Actor {
     onInitialize(engine) {
         this.game = engine;
         //  this.graphics.use(Resources.Fish.toSprite())
-        if(Player.isGoku)
-            {
-                console.log("Its over 9000");
-                let gokuSprite = Resources.Goku.toSprite();
-                gokuSprite.scale = new Vector(0.1,0.1);
-                this.graphics.use(gokuSprite);
-            }
-        else
-        {
-            this.scale= new Vector(0.75,0.75);
+        if (Player.isGoku) {
+            console.log("Its over 9000");
+            let gokuSprite = Resources.Goku.toSprite();
+            gokuSprite.scale = new Vector(0.1, 0.1);
+            this.graphics.use(gokuSprite);
+        }
+        else {
+            this.scale = new Vector(0.75, 0.75);
         }
         this.collider.set(Shape.Box(84, 128, new Vector(0.5, 0.33))) // Makes sure that the Player stand on the platform and doesnt pass through the platform
         this.body.collisionType = CollisionType.Active;
@@ -71,22 +69,21 @@ export class Player extends Actor {
         this.pMove(delta);
         Player.playerPos = this.pos
         Player.playerVel = this.vel;
-      //  console.log(Player.playerVel.y);
-     //   console.log(Player.playerPos);
-        this.pos.x = clamp(this.pos.x,-1385,1385)
+        //  console.log(Player.playerVel.y);
+        //   console.log(Player.playerPos);
+        this.pos.x = clamp(this.pos.x, -1385, 1385)
 
-        if(!Player.isGoku)
-        {
-        this.graphics.use('idle')
-        if (this.vel.y <= 0) {
-            this.graphics.use('jump');
-        } 
-        else if (Math.abs(this.vel.x) > 50) {
-            this.graphics.use('run');
-        }
-        if (this.kbTime > 0) {
-            this.graphics.use('hit')
-        }
+        if (!Player.isGoku) {
+            this.graphics.use('idle')
+            if (this.vel.y <= 0) {
+                this.graphics.use('jump');
+            }
+            else if (Math.abs(this.vel.x) > 50) {
+                this.graphics.use('run');
+            }
+            if (this.kbTime > 0) {
+                this.graphics.use('hit')
+            }
         }
     }
     // Detect player button press
@@ -97,21 +94,21 @@ export class Player extends Actor {
         let gpadX = 0;
         let gpadY = 0;
         let bpress = false;
-        if(Game.gpad!=null){
-        gpadX =  Game.gpad.getAxes(Axes.LeftStickX)
-        gpadY = Game.gpad.getAxes(Axes.LeftStickY)
-        bpress = Game.gpad.isButtonPressed(Buttons.Face1);
+        if (Game.gpad != null) {
+            gpadX = Game.gpad.getAxes(Axes.LeftStickX)
+            gpadY = Game.gpad.getAxes(Axes.LeftStickY)
+            bpress = Game.gpad.isButtonPressed(Buttons.Face1);
         }
-        if (engine.input.keyboard.isHeld(Keys.Left) || engine.input.keyboard.isHeld(Keys.A) || gpadX<0) {
+        if (engine.input.keyboard.isHeld(Keys.Left) || engine.input.keyboard.isHeld(Keys.A) || gpadX < 0) {
             this.xspeed = -500
         }
-        if (engine.input.keyboard.isHeld(Keys.Right) || engine.input.keyboard.isHeld(Keys.D)|| gpadX>0) {
+        if (engine.input.keyboard.isHeld(Keys.Right) || engine.input.keyboard.isHeld(Keys.D) || gpadX > 0) {
             this.xspeed = 500
         }
-        if (engine.input.keyboard.isHeld(Keys.Up) || engine.input.keyboard.isHeld(Keys.W)|| gpadY>0) {
+        if (engine.input.keyboard.isHeld(Keys.Up) || engine.input.keyboard.isHeld(Keys.W) || gpadY < 0) {
             this.yspeed = -1500
         }
-        if (engine.input.keyboard.isHeld(Keys.Down) || engine.input.keyboard.isHeld(Keys.S)|| gpadY<0) {
+        if (engine.input.keyboard.isHeld(Keys.Down) || engine.input.keyboard.isHeld(Keys.S) || gpadY > 0) {
             this.yspeed = 1500
         }
         // When the Spacebar is pressed jump 
@@ -119,15 +116,14 @@ export class Player extends Actor {
     }
     // Calculate and apply velocity
     pMove(delta) {
-      
+
         let xvel = mathFunction.Lerp(this.vel.x, this.xspeed, delta * 0.005);
         let yvel = 0;
-        if(Player.isGoku){
-            yvel= mathFunction.Lerp(this.vel.y,this.yspeed,delta*0.005);
+        if (Player.isGoku) {
+            yvel = mathFunction.Lerp(this.vel.y, this.yspeed, delta * 0.005);
         }
-        else
-        {
-             yvel = mathFunction.Lerp(this.vel.y, 1000, delta * 0.01);
+        else {
+            yvel = mathFunction.Lerp(this.vel.y, 1000, delta * 0.01);
         }
         if (this.doJump && !this.isJumping && Math.abs(this.vel.y) < 850 && this.landed) {
 
@@ -147,12 +143,10 @@ export class Player extends Actor {
                 this.isJumping = false;
             }
         }
-        else
-        {
-            if(Math.abs(this.vel.y)<100)
-                {
-                    this.landed = true;
-                }
+        else {
+            if (Math.abs(this.vel.y) < 100) {
+                this.landed = true;
+            }
         }
         if (this.kbTime <= 0) {
             this.vel = new Vector(xvel, yvel);
